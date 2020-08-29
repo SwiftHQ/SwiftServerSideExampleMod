ffdd660adbfeff15e656bd069e8bd259b5798c50
hayden <haydenv06@gmail.com>
2020-08-30 09:22:48 +1000

    set it up

--- src/main/java/net/swifthq/example/mixin/ExampleMixin.java
+++ src/main/java/net/swifthq/example/mixin/ExampleMixin.java
@@ -1,4 +1,4 @@
-package net.fabricmc.example.mixin;
+package net.swifthq.example.mixin;
 
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
@@ -6,9 +6,9 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(TitleScreen.class)
+@Mixin(TitleScreen.class) // this changes code in the TitleScreen class.
 public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "init()V")
+	@Inject(at = @At("HEAD"), method = "init()V") //this tells mixin to add the method below at the head of the method init()v
 	private void init(CallbackInfo info) {
 		System.out.println("This line is printed by an example mod mixin!");
 	}
